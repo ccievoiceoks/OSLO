d0d3be0062b644a476edf3115a2277746f75592d
Olivier-MBP <olivier@ksiazek.be>
2020-04-10 09:26:10 +0200

    DB Backup

---
+++ SQL/PXS_2020-04-10.sql
@@ -0,0 +1,56 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Hôte: 127.0.0.1 (MySQL 5.5.5-10.4.12-MariaDB-1:10.4.12+maria~bionic)
+# Base de données: PXS
+# Temps de génération: 2020-04-10 07:22:40 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Affichage de la table info
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `info`;
+
+CREATE TABLE `info` (
+  `idinfo` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID Info',
+  `category` varchar(15) NOT NULL DEFAULT '' COMMENT 'Category',
+  `description` varchar(50) NOT NULL COMMENT 'description',
+  PRIMARY KEY (`idinfo`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `info` WRITE;
+/*!40000 ALTER TABLE `info` DISABLE KEYS */;
+
+INSERT INTO `info` (`idinfo`, `category`, `description`)
+VALUES
+	(6,'Cisco','A router can be also a cube'),
+	(7,'Cisco','A switch is not a router'),
+	(8,'ForumConnect','Forum Connect is a new PBX'),
+	(9,'ForumConnect','It is running on a VM'),
+	(10,'Cisco','It is running on a VM');
+
+/*!40000 ALTER TABLE `info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
